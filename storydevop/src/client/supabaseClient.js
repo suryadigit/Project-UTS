2d3003a3bf4e459bb6f7bee8030c665ca7574435
suryadigit <211111397@students.mikroskil.ac.id>
2024-11-15 11:25:28 +0700

    feat: update client api

--- storydevop/src/client/supabaseClient.js
+++ storydevop/src/client/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const SUPABASE_URL = 'https://yhvagkgcvehcbsexctmh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodmFna2djdmVoY2JzZXhjdG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3NzAwNzMsImV4cCI6MjAyOTM0NjA3M30.kvA-v-AunHTELJ9sEyzrFAmYOWlRNKEpKWtQCtvF07U';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
